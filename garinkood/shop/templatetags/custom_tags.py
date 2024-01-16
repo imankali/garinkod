574d910e4fff3cceaccf85b5c5887aaaa904d16f
Imannosrati <105667752+imankali@users.noreply.github.com>
2024-01-15 17:37:48 -0800

    add custom template tags (simpale_tag)

---
+++ garinkood/shop/templatetags/custom_tags.py
@@ -0,0 +1,9 @@
+from django import template
+from ..models import Item
+register = template.Library()
+
+
+@register.simple_tag()
+def number_of_Item():
+    Items = Item.objects.filter(status='published')
+    return "helloworld"
